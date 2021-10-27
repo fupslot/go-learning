f5e5a17aee49208bf2708da6d9571af1bbb787d6
Eugene Brodsky <eugene.brodsky@elastic.co>
2021-10-27 14:24:07 +0300

    collecting file stats

--- file/main.go
+++ file/main.go
@@ -7,14 +7,26 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
+	"os/user"
+	"path/filepath"
+	"strconv"
+	"syscall"
 )
 
 var usageStr = `
 
 Usage "go run main.go in.txt"
 `
+
+type FileResource struct {
+	name  string
+	umask string
+	owner string
+	group string
+	perm  os.FileMode
+	octal string
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -24,13 +36,48 @@ func main() {
 
 	fileName := os.Args[1]
 
-	f, err := os.Open(fileName)
-	if err != nil {
+	fmt.Printf("%s\n", filepath.Base(fileName))
+
+	if info, err := os.Stat(fileName); err == nil {
+		// fmt.Printf("%d\n", int(info.Mode()))
+		// fmt.Printf("%o", info)
+
+		stat := info.Sys().(*syscall.Stat_t)
+		uid := stat.Uid
+		gid := stat.Gid
+
+		// Retriving file's User and Group
+		u, _ := user.LookupId(strconv.FormatUint(uint64(uid), 10))
+		g, _ := user.LookupGroupId(strconv.FormatUint(uint64(gid), 10))
+
+		umask := info.Mode().Perm().String()
+		octal := strconv.FormatUint(uint64(info.Mode().Perm()), 8)
+
+		fmt.Printf("%s %s %s %s %s\n", fileName, umask, octal, u.Username, g.Name)
+
+		fileResource := &FileResource{
+			name:  fileName,
+			umask: umask,
+			octal: octal,
+			perm:  info.Mode().Perm(),
+			owner: u.Username,
+			group: g.Name,
+		}
+
+		fmt.Printf("%+v", fileResource)
+
+	} else if os.IsNotExist(err) {
 		fmt.Println("Error", err)
 		os.Exit(1)
 	}
 
-	defer f.Close()
+	// f, err := os.Open(fileName)
+	// if err != nil {
+	// 	fmt.Println("Error", err)
+	// 	os.Exit(1)
+	// }
 
-	io.Copy(os.Stdin, f)
+	// defer f.Close()
+
+	// io.Copy(os.Stdin, f)
 }
