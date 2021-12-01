ef5900e72a5a240814f80026b6cc473fd4cb7f34
Eugene Brodsky <fupslot@gmail.com>
2021-12-01 15:57:22 +0200

    test byte reader

--- strings/main_test.go
+++ strings/main_test.go
@@ -3,6 +3,7 @@ package main_test
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -16,4 +17,28 @@ func TestMain(t *testing.T) {
 
 	t.Log(fmt.Sprintf("%b", buf.Bytes()))
 	require.Equal(t, buf.Bytes(), []byte("1"))
+}
+
+func TestByteReader(t *testing.T) {
+	var buf bytes.Buffer
+
+	_, err := buf.Write([]byte("Hello World"))
+	require.NoError(t, err)
+
+	r := bytes.NewReader(buf.Bytes())
+	t.Logf("len: %d", r.Len())
+
+	var out bytes.Buffer
+	var b byte
+	for {
+		b, err = r.ReadByte()
+		if err == io.EOF {
+			break
+		}
+		if b == 0x6c {
+			err := out.WriteByte(b)
+			require.NoError(t, err)
+		}
+	}
+	require.Equal(t, out.Bytes(), []byte{'l', 'l', 'l'})
 }
