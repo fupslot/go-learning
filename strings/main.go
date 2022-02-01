93033e18349c68b2f0d13163296a8e731b5dd5da
Eugene Brodsky <fupslot@gmail.com>
2022-02-01 14:31:14 +0200

    working with strings

--- strings/main.go
+++ strings/main.go
@@ -8,7 +8,7 @@ import (
 	"unicode/utf8"
 )
 
-func main() {
+func Playground() {
 	fmt.Println("Contains: ", strings.Contains("test", "st"))
 	fmt.Println("Count:", strings.Count("test", "t"))
 
@@ -38,4 +38,47 @@ func main() {
 		}
 		fmt.Printf("%d\t%c\n", r, r)
 	}
+}
+
+var keypad_map map[rune]int = map[rune]int{
+	'a': 2, 'b': 2, 'c': 2,
+	'd': 3, 'e': 3, 'f': 3,
+	'g': 4, 'h': 4, 'i': 4,
+	'j': 5, 'k': 5, 'l': 5,
+	'm': 6, 'n': 6, 'o': 6,
+	'p': 7, 'q': 7, 'r': 7, 's': 7,
+	't': 8, 'u': 8, 'v': 8,
+	'w': 9, 'x': 9, 'y': 9, 'z': 9,
+}
+
+func toNumber(word string) []int {
+	var n []int = make([]int, len(word))
+	for i := 0; i < len(n); i++ {
+		n[i] = keypad_map[rune(word[i])]
+	}
+	return n
+}
+
+func phoneNumber(letters string) []int {
+	var numbers []int = make([]int, len(letters))
+	for i := 0; i < len(numbers); i++ {
+		numbers[i] = int(rune(letters[i]) - rune('0'))
+	}
+
+	return numbers
+}
+
+func main() {
+	input := []string{"flower", "hello", "tigerking"}
+	fmt.Printf("%v\n", toNumber("hello"))
+	fmt.Printf("%v\n", phoneNumber("470133"))
+
+	var words [][]int = make([][]int, len(input))
+	for i := 0; i < len(words); i++ {
+		words[i] = toNumber(input[i])
+	}
+
+	fmt.Printf("%v", words)
+
+	// playground()
 }
